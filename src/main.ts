bf630554291ce95439426af19f15f94b72012aa9
ISJuanMejia <jcmejiae@siesa.com>
2024-11-21 08:53:10 -0500

    Corregir enrutamiento de la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-import {
-  bootstrapApplication,
-  platformBrowser,
-} from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter, Routes } from "@angular/router";
+import { ValidarUsuarioComponent } from "./app/pages/validar-usuario/validar-usuario.component";
+import { ValidarOtpComponent } from "./app/pages/validar-otp/validar-otp.component";
+import { AppComponent } from "./app/app.component";
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+const routes: Routes = [
+    { path: "validar-usuario", component: ValidarUsuarioComponent },
+    { path: "validar-otp", component: ValidarOtpComponent },
+    { path: "", redirectTo: "validar-usuario", pathMatch: "full" },
+    { path: "**", redirectTo: "validar-usuario" },
+];
+
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
